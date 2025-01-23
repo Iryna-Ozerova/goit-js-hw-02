8b3f4001e0d765ca9a130cb7a39ae786ca6a6ed4
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-01-23 10:27:32 +0200

    fix use strict

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
-`use strict`
+"use strict"
 
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid;
--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,5 @@
-`use strict`
+"use strict" 
+
 function getShippingCost(country) {
     switch (country) {
         case "China":
